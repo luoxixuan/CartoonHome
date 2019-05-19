6a04a44525daa351065a6aabf745fd73c70fb933
luoxixuan <monoluoxixuan@gmail.com>
2019-05-19 17:06:45 +0800

    增加门的控制器

---
+++ Assets/RigidJoint/Scripts/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Cartoon.RigidJoint
+{
+    public class DoorController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] private float k_motorForce = 20.0f;
+        [SerializeField] private float k_motorVel = -25.0f;
+
+        private HingeJoint m_hingeJoint;
+        private JointMotor m_motor = new JointMotor();
+        private JointMotor m_closeMotor = new JointMotor();
+        private bool m_closed = true;
+
+        public void open()
+        {
+            m_hingeJoint.motor = m_motor;
+
+            m_hingeJoint.useMotor = true;
+            m_closed = !m_closed;
+        }
+
+        public void close()
+        {
+            m_hingeJoint.motor = m_closeMotor;
+
+            m_hingeJoint.useMotor = true;
+            m_closed = !m_closed;
+        }
+
+        private void Start()
+        {
+            m_motor.targetVelocity = k_motorVel;
+            m_motor.force = k_motorForce;
+            m_closeMotor.targetVelocity = -k_motorVel;
+            m_closeMotor.force = k_motorForce;
+            m_hingeJoint = gameObject.GetComponent<HingeJoint>();
+
+        }
+    }
+}
--- Assets/RigidJoint/Scripts/OpenDoorTrigger.cs
+++ Assets/RigidJoint/Scripts/OpenDoorTrigger.cs
@@ -4,13 +4,9 @@ namespace Cartoon.RigidJoint
 {
     public class OpenDoorTrigger : RaycastTrigger
     {
-        [SerializeField] private GameObject m_door; //这个门和触发用的物体可以不是一个物体
-        [SerializeField] private float k_motorForce = 20.0f;
-        [SerializeField] private float k_motorVel = -25.0f;
+        private DoorController m_doorController;
 
-        private HingeJoint m_hingeJoint;
-        private JointMotor m_motor = new JointMotor();
-        private JointMotor m_closeMotor = new JointMotor();
+        private bool m_closed = true;
 
         protected override bool init()
         {
@@ -19,17 +15,7 @@ namespace Cartoon.RigidJoint
                 return false;
             }
 
-            m_motor.targetVelocity = k_motorVel;
-            m_motor.force = k_motorForce;
-            m_closeMotor.targetVelocity = -k_motorVel;
-            m_closeMotor.force = -k_motorForce;
-            if (!m_hingeJoint)
-            {
-                //var go = new GameObject("Rigidbody dragger");
-                Rigidbody body = m_door.GetComponent<Rigidbody>();
-                m_hingeJoint = m_door.GetComponent<HingeJoint>();
-                body.isKinematic = true;
-            }
+            m_doorController = gameObject.GetComponent<DoorController>();
 
             return true;
         }
@@ -37,15 +23,16 @@ namespace Cartoon.RigidJoint
         protected override bool doTrigger(RaycastHit hit)
         {
             base.doTrigger(hit);
-            // We need to hit a rigidbody that is not kinematic
-            if (!hit.rigidbody || !hit.rigidbody.isKinematic) //没判断是不是当前物体后续优化
-            {
-                return false;
-            }
 
-            m_door.GetComponent<Rigidbody>().isKinematic = false;
-            m_hingeJoint.motor = m_motor;
-            m_hingeJoint.useMotor = true;
+            if (m_closed)
+            {
+                m_doorController.open();
+            }
+            else
+            {
+                m_doorController.close();
+            }
+            m_closed = !m_closed;
 
             return true;
         }
