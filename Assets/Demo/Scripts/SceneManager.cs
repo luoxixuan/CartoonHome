b4f29a0d9949408cf7a245450ab86e0680cbc2b1
luoxixuan <monoluoxixuan@gmail.com>
2019-05-19 20:54:16 +0800

    调整脚本

--- Assets/Demo/Scripts/SceneManager.cs
+++ Assets/Demo/Scripts/SceneManager.cs
@@ -11,62 +11,33 @@ namespace Cartoon.HomeDemo
         bathroom = 3,
         cat = 4,
     };
+    public enum SkyState
+    {
+        daylight = 0,
+        dust = 1,
+        night = 2,
+    }
 
     public class SceneManager : MonoBehaviour
     {
+#region private
         [SerializeField]
         private GameObject m_girl;
+        [SerializeField]
+        private GameObject m_bedgirl;
         [SerializeField]
         private GameObject m_cat;
         [SerializeField]
         private GameObject m_bathroomCam;
         [SerializeField]
         private GameObject m_bedroomCam;
+        [SerializeField]
+        private Material[] m_skyBox;
 
         private GameState m_state = GameState.start;
+#endregion
 
-        private void Start()
-        {
-
-        }
-        private void StartState()
-        {
-            SwitchState(GameState.bedroom);
-        }
-        private void BedroomtState()
-        {
-            m_girl.SetActive(false);
-            m_bathroomCam.SetActive(false);
-            m_cat.SetActive(false);
-
-            m_bedroomCam.SetActive(true);
-        }
-        private void GirlState()
-        {
-            m_bedroomCam.SetActive(false);
-            m_bathroomCam.SetActive(false);
-            m_cat.SetActive(false);
-
-            m_girl.SetActive(true);
-        }
-        private void BathroomState()
-        {
-            m_girl.SetActive(false);
-            m_bedroomCam.SetActive(false);
-            m_cat.SetActive(false);
-
-            m_bathroomCam.SetActive(true);
-        }
-        private void CatState()
-        {
-            m_girl.SetActive(false);
-            m_bathroomCam.SetActive(false);
-            m_bedroomCam.SetActive(false);
-
-            TriggerObject("Doorhouse");
-            m_cat.SetActive(true);
-        }
-
+#region public
         public void SwitchState(GameState state)
         {
             m_state = state;
@@ -106,7 +77,63 @@ namespace Cartoon.HomeDemo
             if (doorName == "DoorHouse")
             {
                 SwitchState(GameState.cat);
+                SetSkyBox(SkyState.dust);
             }
         }
+
+        public void SetSkyBox(SkyState state)
+        {
+            RenderSettings.skybox = m_skyBox[(int)state];
+        }
+#endregion
+
+#region private
+        private void Start()
+        {
+            SwitchState(GameState.start);
+        }
+        private void StartState()
+        {
+            SwitchState(GameState.bedroom);
+        }
+
+        private void BedroomtState()
+        {
+            m_girl.SetActive(false);
+            m_bathroomCam.SetActive(false);
+            m_cat.SetActive(false);
+
+            m_bedgirl.SetActive(true);
+            m_bedroomCam.SetActive(true);
+        }
+        private void GirlState()
+        {
+            m_bedroomCam.SetActive(false);
+            m_bathroomCam.SetActive(false);
+            m_cat.SetActive(false);
+            m_bedgirl.SetActive(false);
+
+            m_girl.SetActive(true);
+        }
+        private void BathroomState()
+        {
+            m_girl.SetActive(false);
+            m_bedroomCam.SetActive(false);
+            m_cat.SetActive(false);
+            m_bedgirl.SetActive(false);
+
+            m_bathroomCam.SetActive(true);
+        }
+        private void CatState()
+        {
+            m_girl.SetActive(false);
+            m_bathroomCam.SetActive(false);
+            m_bedroomCam.SetActive(false);
+            m_bedgirl.SetActive(false);
+
+            TriggerObject("Doorhouse");
+            m_cat.SetActive(true);
+        }
+#endregion
     }
 }
